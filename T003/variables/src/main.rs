afa634705250c5a36e15e51465ddcbc1301563bd
haruhiko28 <not.hisano.soccer@gmail.com>
2023-12-07 23:09:46 +0900

    3.2 WIP2

--- T003/variables/src/main.rs
+++ T003/variables/src/main.rs
@@ -41,4 +41,44 @@ fn main() {
     let guess: u32 = "42".parse().expect("Not a Number!");
 
     let x = 2.0; // f64    
+
+    let y: f32 = 3.0; // f32
+
+    let sum = 5 + 10;
+    let diff = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let floored = 2/3;
+    let remainder = 43 % 5;
+
+    let t = true;
+
+    let f: bool = false;
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '🐱';
+
+    // タプル
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("the value of y is {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+
+    // 配列
+    let a = [1,2,3,4,5];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    let a: [i32; 5] = [1,2,3,4,5];
+    let a = [3; 5];
+
 }
