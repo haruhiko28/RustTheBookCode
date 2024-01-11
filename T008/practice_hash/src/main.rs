52ffb21232600889c9d26f89b0473c4897d549fc
haruhiko28 <not.hisano.soccer@gmail.com>
2024-01-12 00:24:35 +0900

    DONE CHAP8

--- T008/practice_hash/src/main.rs
+++ T008/practice_hash/src/main.rs
@@ -7,4 +7,29 @@ fn main() {
     scores.insert(String::from("Yellow"), 50);
 
     println!("{}",scores["Blue"]);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    println!("{:?}",score);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    scores.entry(String::from("Red")).or_insert(50);
+    scores.entry(String::from("Pink")).or_insert(50);
+
+    println!("{:?}",scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace(){
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map)
 }
