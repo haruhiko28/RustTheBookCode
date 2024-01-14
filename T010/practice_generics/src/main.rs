1a0494e332a309bad903f04f98a918eabf4bc265
haruhiko28 <not.hisano.soccer@gmail.com>
2024-01-14 23:45:39 +0900

    20240114

---
+++ T010/practice_generics/src/main.rs
@@ -0,0 +1,36 @@
+fn calc_largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    // 最大値は{}です
+    println!("The largest number is {}", largest);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = calc_largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = calc_largest(&number_list);
+    println!("The largest number is {}", result);
+}
