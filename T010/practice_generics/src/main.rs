04dc056ae0ed1580d3197335d1e64eae2f1d3a80
haruhiko28 <not.hisano.soccer@gmail.com>
2024-01-15 23:57:57 +0900

    20240115

--- T010/practice_generics/src/main.rs
+++ T010/practice_generics/src/main.rs
@@ -1,3 +1,5 @@
+use std::alloc::LayoutErr;
+
 fn calc_largest(list: &[i32]) -> i32 {
     let mut largest = list[0];
 
@@ -7,6 +9,17 @@ fn calc_largest(list: &[i32]) -> i32 {
         }
     }
 
+    largest
+}
+
+fn largest_T<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+   
+    for &item in list.iter() {
+        if &item > largest {
+            largest = item;
+        }
+    }
     largest
 }
 
@@ -33,4 +46,14 @@ fn main() {
 
     let result = calc_largest(&number_list);
     println!("The largest number is {}", result);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_T(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_T(&char_list);
+    println!("The largest char is {}", result);
 }
