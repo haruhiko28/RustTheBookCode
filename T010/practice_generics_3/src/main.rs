e47725198aaf414d0a234f3bfc258f082fe601a1
haruhiko28 <not.hisano.soccer@gmail.com>
2024-01-20 22:48:21 +0900

    20240120

--- T010/practice_generics_3/src/main.rs
+++ T010/practice_generics_3/src/main.rs
@@ -2,9 +2,12 @@
 
 pub trait Summary {
     // fn summarize(&self) -> String;
+
+    fn summarize_author(&self) -> String;
+
     fn summarize(&self) -> String {
-        // "（もっと読む）"
-        String::from("(Read more...)")
+        // "（{}さんの文章をもっと読む）"
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
 pub struct NewsArticle {
@@ -17,6 +20,9 @@ pub struct NewsArticle {
 impl Summary for NewsArticle {
     fn summarize(&self) -> String {
         format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
     }
 }
 
@@ -31,7 +37,62 @@ impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
     }
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
 }
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+fn returns_summarizable(switch: bool) -> impl Summary {
+    if switch {
+        NewsArticle {
+            headline: String::from(
+                "Penguins win the Stanley Cup Championship! 1",
+            ),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        }
+    } else {
+        NewsArticle {
+            headline: String::from(
+                "Penguins win the Stanley Cup Championship! 2",
+            ),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        }
+        // Tweet {
+        //     username: String::from("horse_ebooks"),
+        //     content: String::from("of course, as you probably already know, people"),
+        //     reply: false,
+        //     retweet: false,
+        // }
+    }
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+
 fn main() {
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
@@ -59,4 +120,14 @@ fn main() {
     };
 
     println!("New article available! {}", article.summarize());
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
