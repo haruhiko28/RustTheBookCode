1185457cc208befc6b10290babce7e7ef30fbfd8
haruhiko28 <not.hisano.soccer@gmail.com>
2024-01-30 23:52:26 +0900

    20240130

--- T011/adder/src/lib.rs
+++ T011/adder/src/lib.rs
@@ -14,6 +14,45 @@ impl Rectangle {
     }
 }
 
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+// impl Guess {
+//     pub fn new(value: i32) -> Guess {
+//         if value < 1 || value > 100 {
+//             //予想値は1から100の間でなければなりませんが、{}でした。
+//             panic!("Guess value must be between 1 and 100, got {}.", value);
+//         }
+
+//         Guess { value }
+//     }
+// }
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                //予想値は1以上でなければなりませんが、{}でした。
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                //予想値は100以下でなければなりませんが、{}でした。
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -59,4 +98,39 @@ mod tests {
         assert!(!smaller.can_hold(&larger));
     }
 
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"));
+
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            //挨拶(greeting)は名前を含んでいません。その値は`{}`でした
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    // #[test]
+    // #[should_panic]
+    // fn greater_than_100() {
+    //     Guess::new(200);
+    // }
+
+    #[test]
+    //予想値は100以下でなければなりません
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn it_works_2() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
 }
