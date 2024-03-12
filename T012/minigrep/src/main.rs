db1c95a9b3cf467cad0d437bb92530d12e990613
haruhiko28 <not.hisano.soccer@gmail.com>
2024-03-12 23:58:39 +0900

    by 12-9

--- T012/minigrep/src/main.rs
+++ T012/minigrep/src/main.rs
@@ -2,21 +2,36 @@ use std::{env, fs::File, io::Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let query = &args[1];
-    let filename = &args[2];
 
-    // {}を探しています
-    println!("Searching for {}", query);
-    // {}というファイルの中
-    println!("In file {}", filename);
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
     // ファイルが見つかりませんでした
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
     .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
